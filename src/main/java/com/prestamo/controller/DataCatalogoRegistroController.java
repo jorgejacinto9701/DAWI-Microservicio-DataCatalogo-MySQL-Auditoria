a7a7fe2dcb9d62c5b8309225eb8ffcf2154b539f
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-10-01 16:51:54 -0500

    ajustes

--- src/main/java/com/prestamo/controller/DataCatalogoRegistroController.java
+++ src/main/java/com/prestamo/controller/DataCatalogoRegistroController.java
@@ -3,15 +3,19 @@ package com.prestamo.controller;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prestamo.entity.DataCatalogo;
@@ -49,15 +53,65 @@ public class DataCatalogoRegistroController {
 		return ResponseEntity.ok(salida);
 	}
 
-	@GetMapping("/validaDescripcionRegistra")
-	public String validaDescripcion(@RequestParam(name = "descripcion") String descripcion) {
-		List<DataCatalogo> lstSalida = dataCatalogoService.listaDataCatalogoPorDescripcionIgual(descripcion);
-		if (lstSalida.isEmpty()) {
-			return "{\"valid\":true}";
-		} else {
-			return "{\"valid\":false}";
+	
+	@PostMapping("/registraDataCatalogo")
+	@ResponseBody
+	public ResponseEntity<?> insertaDataCatalogo(@RequestBody DataCatalogo obj) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			obj.setIdDataCatalogo(0);
+			obj.setFechaActualizacion(new Date());
+			obj.setFechaRegistro(new Date());
+			obj.setEstado(AppSettings.ACTIVO);
+			
+			DataCatalogo objSalida = dataCatalogoService.insertaActualizaDataCatalogo(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", AppSettings.MENSAJE_REG_ERROR);
+			} else {
+				salida.put("mensaje", AppSettings.MENSAJE_REG_EXITOSO + " Data Catálogo de ID ==> " + obj.getIdDataCatalogo() + ".");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", AppSettings.MENSAJE_REG_ERROR);
 		}
-
+		return ResponseEntity.ok(salida);
 	}
+
+	@PutMapping("/actualizaDataCatalogo")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> actualizaDataCatalogo(@RequestBody DataCatalogo obj) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			
+			obj.setFechaActualizacion(new Date());
+
+			DataCatalogo objSalida = dataCatalogoService.insertaActualizaDataCatalogo(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", AppSettings.MENSAJE_ACT_ERROR);
+			} else {
+				salida.put("mensaje", AppSettings.MENSAJE_ACT_EXITOSO + " Data catálogo de ID ==> " + obj.getIdDataCatalogo() + ".");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", AppSettings.MENSAJE_ACT_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+	
+	@DeleteMapping("/eliminaDataCatalogo/{id}")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> eliminaDataCatalogo(@PathVariable("id") int idDataCatalogo) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			dataCatalogoService.eliminaDataCatalogo(idDataCatalogo);
+			salida.put("mensaje", AppSettings.MENSAJE_ELI_EXITOSO + " Data catálogo de ID ==> " + idDataCatalogo + ".");
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", AppSettings.MENSAJE_ELI_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
 
 }
